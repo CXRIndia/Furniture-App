205caa8cb8c060ada694391c9565687723835ed1
akshay-cemtrexlabs <apatil@cemtrexlabs.com>
2020-11-26 17:59:22 +0530

    initial commit

---
+++ FurnitureAR/Helper/InstantPanGestureRecognizer.swift
@@ -0,0 +1,23 @@
+//
+//  InstantPanGestureRecognizer.swift
+//  FurnitureAR
+//
+//  Created by akshay patil on 14/02/20.
+//  Copyright © 2020 akshay patil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - InstantPanGestureRecognizer
+
+/// A pan gesture that enters into the `began` state on touch down instead of waiting for a touches moved event.
+class InstantPanGestureRecognizer: UIPanGestureRecognizer {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        if (self.state == UIGestureRecognizer.State.began) { return }
+        super.touchesBegan(touches, with: event)
+        self.state = UIGestureRecognizer.State.began
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FurnitureAR' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FurnitureAR
+    pod 'TPKeyboardAvoiding'
+    pod 'ObjectMapper'
+    pod 'RealmSwift'
+    pod 'SwiftyGif'
+
+  target 'FurnitureARTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FurnitureARUITests' do
+    # Pods for testing
+  end
+
+end
